65a8b8f02bf65daa879b4cd627f232e4b0b008a9
mlic <mlic>
2006-02-08 17:16:04 +0000

    allgemein gehaltenes Interface fuer alle Algorithem in FSAAlgo

---
+++ src/algo/GenericAlgorithm.java
@@ -0,0 +1,17 @@
+/*
+ * GenericAlgorithm.java
+ *
+ * Created on 7. Februar 2006, 15:38
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package algo;
+
+public interface GenericAlgorithm {
+
+    public void runAlgorithm();
+    public Object getResult();
+    
+}
